8b649ef4340676c83cb2c0797049d4918840ab18
Yaroslav <rykal2014@gmail.com>
2018-04-09 12:18:17 -0400

    Update makeArrayConsecutive2.js

--- makeArrayConsecutive2.js
+++ makeArrayConsecutive2.js
@@ -1,4 +1,9 @@
 function makeArrayConsecutive2(statues) {
-
+    
     return Math.max(...statues) - Math.min(...statues) - statues.length + 1;    
 }
+
+//No need to sort! 
+//Just find out max and min in array. 
+//Calculate how much must be inside and compare with length of start array! 
+//Just add something))
