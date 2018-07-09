7c27471eaf4b8801fc71eed8172bf76b5b8a1cc9
Yaroslav <rykal2014@gmail.com>
2018-07-09 08:40:57 -0400

    Create isDigit.js
    Determine if the given character is a digit or not.

---
+++ isDigit.js
@@ -0,0 +1,9 @@
+function isDigit(symbol) {
+    
+    var match = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    
+    if (match.includes(symbol))
+        return true;
+    else 
+        return false; 
+}
